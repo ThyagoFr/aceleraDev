7213b25476e533008877125776600f840b1fc254
ThyagoFr <thyagofr@alu.ufc.br>
2020-06-23 21:49:14 -0300

    add put method

--- springboot/springmongodb/src/main/java/com/thyagofr/springmongodb/resources/UserResource.java
+++ springboot/springmongodb/src/main/java/com/thyagofr/springmongodb/resources/UserResource.java
@@ -1,4 +1,5 @@
 package com.thyagofr.springmongodb.resources;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import com.thyagofr.springmongodb.domain.User;
@@ -7,9 +8,11 @@ import com.thyagofr.springmongodb.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -32,6 +35,28 @@ public class UserResource {
   public ResponseEntity<UserDTO> findByID(@PathVariable String id) {
     User user = service.findByID(id);
     return ResponseEntity.ok().body(new UserDTO(user));
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Void> insert(@RequestBody UserDTO userdto) {
+    User user = service.fromDTOP(userdto);
+    user = service.insert(user);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userdto) {
+    User user = service.fromDTOP(userdto);
+    user.setId(id);
+    user = service.update(user);
+    return ResponseEntity.noContent().build();
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public ResponseEntity<Void> delete(@PathVariable String id){
+    service.delete(id);
+    return ResponseEntity.noContent().build();
   }
   
 }
--- springboot/springmongodb/src/main/java/com/thyagofr/springmongodb/services/UserServices.java
+++ springboot/springmongodb/src/main/java/com/thyagofr/springmongodb/services/UserServices.java
@@ -2,6 +2,7 @@ package com.thyagofr.springmongodb.services;
 import java.util.List;
 import java.util.Optional;
 import com.thyagofr.springmongodb.domain.User;
+import com.thyagofr.springmongodb.dto.UserDTO;
 import com.thyagofr.springmongodb.repository.UserRepository;
 import com.thyagofr.springmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -22,5 +23,30 @@ public class UserServices {
     Optional<User> user = repo.findById(id);
     return user.orElseThrow( () -> new ObjectNotFoundException("Objeto nao encontrado"));
   }
+
+  public User insert(User user) {
+    return repo.insert(user);
+  }
+
+  public void delete(String id){
+    findByID(id);
+    repo.deleteById(id);
+  }
+
+  public User update(User user){
+    findByID(user.getId());
+    Optional<User> newuser = repo.findById(user.getId());
+    updateUser(newuser.get(),user);
+    return repo.save(newuser.get());
+  }
   
+  private void updateUser(User newuser, User olduser){
+    newuser.setEmail(olduser.getEmail());
+    newuser.setName(olduser.getName());
+  } 
+
+  public User fromDTOP(UserDTO userDTO) {
+    return new User(userDTO.getId(),userDTO.getName(),userDTO.getEmail());
+  }
+
 }
