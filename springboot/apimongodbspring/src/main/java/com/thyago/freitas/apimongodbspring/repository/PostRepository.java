565983c933cf4d21752b7b6b4f58091af04e4558
ThyagoFr <thyagofr@alu.ufc.br>
2020-08-02 17:32:47 -0300

    voltando...

--- springboot/apimongodbspring/src/main/java/com/thyago/freitas/apimongodbspring/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thyago.freitas.apimongodbspring.repository;
-import com.thyago.freitas.apimongodbspring.models.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends MongoRepository<Post, String> {
-}
--- springboot/apimongodbspring/src/main/java/com/thyago/freitas/apimongodbspring/services/exceptions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.thyago.freitas.apimongodbspring.services.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException{
-    private static final long serialVersion = 1L;
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
